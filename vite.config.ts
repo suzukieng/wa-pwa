89b7041af1435f98839bb7576652f33f1db0d1b4
Alex Suzuki <alex@pixelverse.ch>
2025-04-17 17:39:16 +0900

    feat: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    minify: false,
+    cssMinify: false
+  },
+  plugins: [VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+    minify: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'wa-pwa',
+      short_name: 'wa-pwa',
+      description: 'WebAudio Test PWA',
+      theme_color: '#0f2a42',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
